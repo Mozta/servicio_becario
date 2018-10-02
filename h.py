33e6396587e3bf1f13531590d7affdb860e1a1ea
UzielMAnd <42781097+UzielMAnd@users.noreply.github.com>
2018-10-02 12:53:18 -0500

    Add files via upload

---
+++ h.py
@@ -0,0 +1,95 @@
+archivo=open("Axolotl.txt","r+")
+content=archivo.read()
+finDelArchivo=archivo.tell()
+
+archivo.write("El azar me llevó hasta ellos una mañana de primavera en que París abría su cola de pavo real después de la lenta invernada. Bajé por el bulevar de Port Royal, tomé St. Marcel y L’Hôpital, vi los verdes entre tanto gris y me acordé de los leones. Era amigo de los leones y las panteras, pero nunca había entrado en el húmedo y oscuro edificio de los acuarios. Dejé mi bicicleta contra las rejas y fui a ver los tulipanes. Los leones estaban feos y tristes y mi pantera dormía. Opté por los acuarios, soslayé peces vulgares hasta dar inesperadamente con los axolotl. Me quedé una hora mirándolos, y salí incapaz de otra cosa.El azar me llevó hasta ellos una mañana de primavera en que París abría su cola de pavo real después de la lenta invernada. Bajé por el bulevar de Port Royal, tomé St. Marcel y L’Hôpital, vi los verdes entre tanto gris y me acordé de los leones. Era amigo de los leones y las panteras, pero nunca había entrado en el húmedo y oscuro edificio de los acuarios. Dejé mi bicicleta contra las rejas y fui a ver los tulipanes. Los leones estaban feos y tristes y mi pantera dormía. Opté por los acuarios, soslayé peces vulgares hasta dar inesperadamente con los axolotl. Me quedé una hora mirándolos, y salí incapaz de otra cosa.El azar me llevó hasta ellos una mañana de primavera en que París abría su cola de pavo real después de la lenta invernada. Bajé por el bulevar de Port Royal, tomé St. Marcel y L’Hôpital, vi los verdes entre tanto gris y me acordé de los leones. Era amigo de los leones y las panteras, pero nunca había entrado en el húmedo y oscuro edificio de los acuarios. Dejé mi bicicleta contra las rejas y fui a ver los tulipanes. Los leones estaban feos y tristes y mi pantera dormía. Opté por los acuarios, soslayé peces vulgares hasta dar inesperadamente con los axolotl. Me quedé una hora mirándolos, y salí incapaz de otra cosa.El azar me llevó hasta ellos una mañana de primavera en que París abría su cola de pavo real después de la lenta invernada. Bajé por el bulevar de Port Royal, tomé St. Marcel y L’Hôpital, vi los verdes entre tanto gris y me acordé de los leones. Era amigo de los leones y las panteras, pero nunca había entrado en el húmedo y oscuro edificio de los acuarios. Dejé mi bicicleta contra las rejas y fui a ver los tulipanes. Los leones estaban feos y tristes y mi pantera dormía. Opté por los acuarios, soslayé peces vulgares hasta dar inesperadamente con los axolotl. Me quedé una hora mirándolos, y salí incapaz de otra cosa.El azar me llevó hasta ellos una mañana de primavera en que París abría su cola de pavo real después de la lenta invernada. Bajé por el bulevar de Port Royal, tomé St. Marcel y L’Hôpital, vi los verdes entre tanto gris y me acordé de los leones. Era amigo de los leones y las panteras, pero nunca había entrado en el húmedo y oscuro edificio de los acuarios. Dejé mi bicicleta contra las rejas y fui a ver los tulipanes. Los leones estaban feos y tristes y mi pantera dormía. Opté por los acuarios, soslayé peces vulgares hasta dar inesperadamente con los axolotl. Me quedé una hora mirándolos, y salí incapaz de otra cosa.El azar me llevó hasta ellos una mañana de primavera en que París abría su cola de pavo real después de la lenta invernada. Bajé por el bulevar de Port Royal, tomé St. Marcel y L’Hôpital, vi los verdes entre tanto gris y me acordé de los leones. Era amigo de los leones y las panteras, pero nunca había entrado en el húmedo y oscuro edificio de los acuarios. Dejé mi bicicleta contra las rejas y fui a ver los tulipanes. Los leones estaban feos y tristes y mi pantera dormía. Opté por los acuarios, soslayé peces vulgares hasta dar inesperadamente con los axolotl. Me quedé una hora mirándolos, y salí incapaz de otra cosa.")
+
+archivo.seek(finDelArchivo)
+newContent=archivo.read()
+
+i=0
+Palabras=("a,ante,bajo,con,de,desde,durante,en,entre,excepto,hacia,hasta,mediante,para,por,salvo,segun,sin,sobre,tras")
+
+while Palabras[0]==True:
+    i=i+1
+    print("La cantidad de preposiciones 'a' es:",i)
+    break
+for Palabras in[1]:
+    i=i+1
+    print("La cantidad de preposiciones 'ante' es:",i)
+    break
+for Palabras in[2]:
+    i=i+1
+    print("La cantidad de preposiciones 'bajo' es:",i)
+    break
+for Palabras in[3]:
+    i=i+1
+    print("La cantidad de preposiciones 'con' es:",i)
+    break
+for Palabras in[4]:
+    i=i+1
+    print("La cantidad de preposiciones 'de' es:",i)
+    break
+for Palabras in[5]:
+    i=i+1
+    print("La cantidad de preposiciones 'desde' es:",i)
+    break
+for Palabras in[6]:
+    i=i+1
+    print("La cantidad de preposiciones 'adurante' es:",i)
+    break
+for Palabras in[7]:
+    i=i+1
+    print("La cantidad de preposiciones 'en' es:",i)
+    break
+for Palabras in[8]:
+    i=i+1
+    print("La cantidad de preposiciones 'entre' es:",i)
+    break
+for Palabras in[9]:
+    i=i+1
+    print("La cantidad de preposiciones 'excepto' es:",i)
+    break
+for Palabras in[10]:
+    i=i+1
+    print("La cantidad de preposiciones 'hacia' es:",i)
+    break
+for Palabras in[11]:
+    i=i+1
+    print("La cantidad de preposiciones 'hasta' es:",i)
+    break
+for Palabras in[12]:
+    i=i+1
+    print("La cantidad de preposiciones 'mediante' es:",i)
+    break
+for Palabras in[13]:
+    i=i+1
+    print("La cantidad de preposiciones 'para' es:",i)
+    break
+for Palabras in[14]:
+    i=i+1
+    print("La cantidad de preposiciones 'por' es:",i)
+    break
+for Palabras in[15]:
+    i=i+1
+    print("La cantidad de preposiciones 'salvo' es:",i)
+    break
+for Palabras in[16]:
+    i=i+1
+    print("La cantidad de preposiciones 'segun' es:",i)
+    break
+for Palabras in[17]:
+    i=i+1
+    print("La cantidad de preposiciones 'sin' es:",i)
+    break
+for Palabras in[18]:
+    i=i+1
+    print("La cantidad de preposiciones 'sobre' es:",i)
+    break
+for Palabras in[19]:
+    i=i+1
+    print("La cantidad de preposiciones 'tras' es:",i)
+    break
+
+archivo.close()
+print (newContent)
